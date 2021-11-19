5e2c7ff431f9b0480ef658a992d06c5954ebc0aa
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-11-19 17:35:10 +0530

    Area of a Triangle

--- practical4.cpp
+++ practical4.cpp
@@ -6,10 +6,10 @@ void main()
 {
     clrscr();
     float s1,s2,s3;
-    float s,area;
+    float area;
     cout << "Enter the three sides : ";
     cin >> s1 >> s2 >> s3;
-    s = (s1+s2+s3)/2;
+    float s = (s1+s2+s3)/2;
     area = sqrt(s*(s-s1)*(s-s2)*(s-s3));
     cout << "Area = " << area << endl;
     getch();
