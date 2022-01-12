4cd59640a3d0ae34875ab7772b37618e4a7b2d1f
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2022-01-12 21:02:20 +0530

    Electricity bill

--- practical9.cpp
+++ practical9.cpp
@@ -9,3 +9,30 @@ Units Consumed Cost
 >=100 Rs 5.85 /unit
 @ChaitanyaJoshiX
 */
+#include <iostream.h>
+#include <conio.h>
+void main()
+{
+    getch();
+    double elecunits,amount;
+    cout << "Enter the number of units consumed : ";
+    cin >> elecunits;
+    if(elecunits < 30)
+    {
+        amount = elecunits * 3.50;
+    }
+    else if(elecunits >= 30 && elecunits < 50)
+    {
+        amount = (29 * 3.50) + (elecunits - 29) * 4.25;
+    }
+    else if(elecunits >= 50 && elecunits < 100)
+    {
+        amount = (29 * 3.50) + (19 * 4.25) + (elecunits - 49) * 5.25;
+    }
+    else
+    {
+        amount = (29 * 3.50) + (19 * 4.25) + (49 * 5.25) + (elecunits - 99) * 5.85;
+    }
+    cout << "Final amount = " << amount << endl;
+    getch();
+}
